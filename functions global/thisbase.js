d624a732106c989c2685d697abc1e9cf7e8fc740
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2022-12-05 02:44:32 +0800

    Added word counter

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -1,6 +1,6 @@
 export const thisbase =()=>{
     var url = 
-    "http://localhost:3000/api"
-    // 'https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api'
+    // "http://localhost:3000/api"
+    'https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api'
     return url
 }
