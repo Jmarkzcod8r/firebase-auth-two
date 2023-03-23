c2bf93895058682b70d0dc896117324466e79dc8
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-03-23 20:45:23 +0800

    Normalize api for online

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -1,5 +1,5 @@
 export const thisbase = () => {
-  //   var url = "http://localhost:3000/api";
+  //   var url = "http://localhost:3000/api";/
   "https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api";
   // 'https://www.jmwebgo.com/api'
   return url;
