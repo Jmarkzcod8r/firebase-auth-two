0e598c2a580b6d72d290678f7c2293afdac83b42
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-03-24 01:34:57 +0800

    Took out

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -1,5 +1,4 @@
 export const thisbase = () => {
-  //   var url = "http://localhost:3000/api";/
   "https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api";
   // 'https://www.jmwebgo.com/api'
   return url;
