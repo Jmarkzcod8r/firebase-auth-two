36469de8ef0355ccb63cffe8092ebe88d8034547
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-07-03 21:55:53 +0800

    Configured url to www.devjourn.com/api

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -1,6 +1,6 @@
 export const thisbase = () => {
   // var url = "https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api";
-  var url = "https://www.devjourn.com"
+  var url = "https://www.devjourn.com/api"
   // var url = "http://localhost:3000/api"
   // 'https://www.jmwebgo.com/api'
   return url;
