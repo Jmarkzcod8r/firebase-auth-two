7f463eac610c04a8c35ae538184d176c449e595e
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-12-14 10:20:59 +0800

    Take out console.logs on back end

--- pages/api/clients/query.js
+++ pages/api/clients/query.js
@@ -9,12 +9,12 @@ export default async function QueryPage(req, res) {
   switch (method) {
     case "POST":
       try {
-        console.log('searchTerm:', searchTerm)
+        // console.log('searchTerm:', searchTerm)
         // const clients = await ModelFeed.find({
           const clients = await mongoose.models.logdata.find({
           log: { $regex: searchTerm }, // Search for 'Dude' (case insensitive)
         });
-        console.log(clients)
+        // console.log(clients)
         res.status(200).json({ success: true, data: clients }); // Return the data as JSON
       } catch (error) {
         console.error(error);
