ebb6f7ace7d01ed7c4608baca7817d4be32f6df3
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2023-12-14 11:10:48 +0800

    Adjusted on thisbase.js

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -1,8 +1,8 @@
 export const thisbase = () => {
   // var url = "https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api";
 
-  // var url = "https://www.devjourn.com/api"
-  var url = "http://www.devjourn.com/api"
+  var url = "https://www.devjourn.com/api"
+  // var url = "http://www.devjourn.com/api"
   // var url = "http://localhost:3000/api"
   // 'https://www.jmwebgo.com/api'
   // var url = "http://localhost:3001/api"
