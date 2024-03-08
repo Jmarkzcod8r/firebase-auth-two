6dacadd2d585e0d6caf53e933046ca52f5b3c9a7
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2024-03-08 19:54:05 +0800

    change url in thisbase.js to devjourn

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -2,8 +2,8 @@ export const thisbase = () => {
   // var url = "https://firebase-auth-two-new-jmarkzcod8r.vercel.app/api";
 
   // var url = "https://www.devjourn.com/api"
-  // var url = "http://www.devjourn.com/api"
-  var url = "http://localhost:3000/api"
+  var url = "http://www.devjourn.com/api"
+  // var url = "http://localhost:3000/api"
   // 'https://www.jmwebgo.com/api'
   // var url = "http://localhost:3001/api"
   return url;
