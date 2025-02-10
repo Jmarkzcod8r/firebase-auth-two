a9ed7777f262520a6aae0dedf203ee274bae2669
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2025-02-10 15:47:41 +0800

    Fixes..

--- functions global/thisbase.js
+++ functions global/thisbase.js
@@ -3,10 +3,10 @@ export const thisbase = () => {
 
   // var url = "https://www.devjourn.com/api"
   // var url = "http://www.devjourn.com/api"
-  var url = "http://localhost:3000/api"
+  // var url = "http://localhost:3000/api"
   // 'https://www.jmwebgo.com/api'
   // var url = "http://localhost:3001/api"
 
-  // var url = "https://jmdevjourn.netlify.app/api"
+  var url = "https://jmdevjourn.netlify.app/api"
   return url;
 };
